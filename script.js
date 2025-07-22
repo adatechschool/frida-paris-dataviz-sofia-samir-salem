393db7cfeb14a92079555fdf35fd19fafae07c9f
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-07-22 16:28:59 +0200

    add aler allergens function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const showInformation =document.getElementById("affichageinfo")
-const productCode= 9002515600530
+const productCode= /* 9002515600530 3497911219121 3017624010701 3017760038331*/  3017760038331
 async function getInformation(){
     const response = await fetch(`https://world.openfoodfacts.net/api/v2/product/${productCode}`)
     const data = await response.json()
@@ -25,7 +25,7 @@ async function displayIngredients() {
     
         const product = await getInformation()
         const ingredients = document.createElement('p')
-        ingredients.innerText = (`Ingridient du ${product.product.product_name} : \n\n ${product.product.ingredients_text}`)
+        ingredients.innerText = (`Ingredient : \n\n ${product.product.ingredients_text}`)
         showInformation.appendChild(ingredients)
 
 }
@@ -39,3 +39,17 @@ async function displayGrade() {
 }
 
 displayGrade()
+
+
+async function alertAllergens() {
+    const product = await getInformation()
+    const allergens = document.createElement('p')
+    if(product.product.allergens_from_ingredients === ''){
+        allergens.innerText = "produit ne contient pas d'allergens"
+    }else{
+        allergens.innerText=` Les ingredient allegens du produit : \n ${product.product.allergens_from_ingredients}`
+    }
+    showInformation.appendChild(allergens)
+}
+
+alertAllergens()
