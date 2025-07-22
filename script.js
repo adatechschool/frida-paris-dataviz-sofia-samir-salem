4a7d2e30740acdc061de15fd6787e4657d2737a7
Sosow20 <sofiacherifi1995@gmail.com>
2025-07-22 16:28:06 +0200

    function  packaging

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 
-const showInformation =document.getElementById("affichageinfo")
-const productCode= 9002515600530
-async function getInformation(){
+const showInformation = document.getElementById("affichageinfo")
+const productCode = 3257971309114
+async function getInformation() {
     const response = await fetch(`https://world.openfoodfacts.net/api/v2/product/${productCode}`)
     const data = await response.json()
     return (data)
 }
 async function displayImg() {
-    const product=await getInformation()
+    const product = await getInformation()
     console.log(product)
-    const img= document.createElement('img')
-    img.src=product.product.image_front_small_url
+    const img = document.createElement('img')
+    img.src = product.product.image_front_small_url
     showInformation.appendChild(img)
-     
+
 }
-
-
-
 
 displayImg()
 
 
 async function displayIngredients() {
-    
-        const product = await getInformation()
-        const ingredients = document.createElement('p')
-        ingredients.innerText = (`Ingridient du ${product.product.product_name} : \n\n ${product.product.ingredients_text}`)
-        showInformation.appendChild(ingredients)
+
+    const product = await getInformation()
+    const ingredients = document.createElement('p')
+    ingredients.innerText = (`Ingrédients du ${product.product.product_name} : \n\n ${product.product.ingredients_text}`)
+    showInformation.appendChild(ingredients)
 
 }
 displayIngredients()
@@ -39,3 +36,22 @@ async function displayGrade() {
 }
 
 displayGrade()
+
+async function packaging() {
+    const product = await getInformation()
+    const emballage = document.getElementById('emballage')
+    // packaging.innerText = (`Type d'emballage : \n ${product.product.packaging_hierarchy} `)
+    console.log(product.product.packaging_tags[1])
+    for (let i = 0; i < product.product.packaging_tags.length; i++) {
+        const packaging = document.createElement('li')
+        if (product.product.packaging_tags[i].startsWith("fr")) {
+            packaging.innerHTML = (` \n ${product.product.packaging_tags[i].slice(5)} `)
+        }
+        emballage.appendChild(packaging)
+    }
+
+}
+
+packaging()
+
+
