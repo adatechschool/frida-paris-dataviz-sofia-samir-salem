dbaa2177f5901a325dc24f5b2f864a85de434328
Samir <samirelorf@yahoo.fr>
2025-07-24 12:26:32 +0200

    maj code

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const showInformation = document.getElementById("affichageinfo")
+const showInformation =document.getElementById("affichageinfo")
 // const productCode= /* 9002515600530 3497911219121 3017624010701 3017760038331*/  3017624010701
 //const productCode= /* 9002515600530 3497911219121 3017624010701 3017760038331 3257971309114 */   3017624010701
 const btnStart = document.getElementById("startcamera")
 const cameraResultat = document.getElementById("cameraresult")
 const displayCamera = document.getElementById('zonecamera')
 
-// btnStart.addEventListener('click', () => {
-//   displayCamera.style.display = 'block'
-//   Quagga.init({
-//     inputStream: {
-//       name: "Live",
-//       type: "LiveStream",
-//       target: document.querySelector('#zonecamera'),
-//       constraints: { facingMode: "environment" }
-//     },
-//     decoder: {
-//       readers: ["ean_reader", "upc_reader", "code_128_reader"]
-//     }
-//   }, function (err) {
-//     if (err) {
-//       console.log(err);
-//       return
-//     }
-//     console.log("Initialization finished. Ready to start");
-//     Quagga.start();
-//   })
-//   Quagga.onDetected(resultat => {
-//     code = resultat.codeResult.code
-//     cameraResultat.innerText = `le code barre detecte : ${code}`
-//     Quagga.stop()
-//     displayCamera.style.display = 'none'
+btnStart.addEventListener('click',()=>{
+    displayCamera.style.display='block'
+    Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#zonecamera') ,
+      constraints: {facingMode: "environment"}
+    },
+    decoder : {
+      readers : ["ean_reader","upc_reader","code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  })
+  Quagga.onDetected(resultat=>{
+    code = resultat.codeResult.code
+    cameraResultat.innerText= `le code barre detecte : ${code}`
+    Quagga.stop()
+    displayCamera.style.display= 'none'
 
     displayImg(code)
     displayIngredients(code)
@@ -43,17 +43,17 @@ const displayCamera = document.getElementById('zonecamera')
 })
 
 
-async function getInformation(productCode) {
-  const response = await fetch(`https://world.openfoodfacts.net/api/v2/product/${productCode}`)
-  const data = await response.json()
-  return (data)
+async function getInformation(productCode){
+    const response = await fetch(`https://world.openfoodfacts.net/api/v2/product/${productCode}`)
+    const data = await response.json()
+    return (data)
 }
 async function displayImg(productCode) {
-  const product = await getInformation(productCode)
-  console.log(product)
-  const img = document.createElement('img')
-  img.src = product.product.image_front_small_url
-  showInformation.appendChild(img)
+    const product = await getInformation(productCode)
+    console.log(product)
+    const img = document.createElement('img')
+    img.src = product.product.image_front_small_url
+    showInformation.appendChild(img)
 
 }
 
@@ -61,36 +61,25 @@ async function displayImg(productCode) {
 
 
 async function displayIngredients(productCode) {
-
-  const product = await getInformation(productCode)
-  const ingredients = document.createElement('p')
-  ingredients.innerText = (`Ingredient : \n\n ${product.product.ingredients_text}`)
-  showInformation.appendChild(ingredients)
+    
+        const product = await getInformation(productCode)
+        const ingredients = document.createElement('p')
+        ingredients.innerText = (`Ingredient : \n\n ${product.product.ingredients_text}`)
+        showInformation.appendChild(ingredients)
 
 }
 //displayIngredients()
 
 async function displayGrade(productCode) {
-  const product = await getInformation(productCode)
-  const grade = document.createElement('p')
-  grade.innerText = (`Grade : \n ${product.product.nutriscore_grade}`)
-  showInformation.appendChild(grade)
+    const product = await getInformation(productCode)
+    const grade = document.createElement('p')
+    grade.innerText = (`Grade : \n ${product.product.nutriscore_grade}`)
+    showInformation.appendChild(grade)
 }
 
 //displayGrade()
 
 async function packaging(productCode) {
-  const product = await getInformation(productCode)
-  const emballage = document.getElementById('emballage')
-  // packaging.innerText = (`Type d'emballage : \n ${product.product.packaging_hierarchy} `)
-  console.log(product.product.packaging_tags[1])
-  for (let i = 0; i < product.product.packaging_tags.length; i++) {
-    const packaging = document.createElement('li')
-    if (product.product.packaging_tags[i].startsWith("fr")) {
-      packaging.innerHTML = (` \n ${product.product.packaging_tags[i].slice(3)} `)
-    }
-    emballage.appendChild(packaging)
-  }
     const product = await getInformation(productCode)
     const emballage = document.getElementById('emballage')
     // packaging.innerText = (`Type d'emballage : \n ${product.product.packaging_hierarchy} `)
@@ -111,24 +100,20 @@ async function packaging(productCode) {
     }
    
 
-    }
-  }
-
-
 }
 
 //packaging()
 
 
 async function alertAllergens(productCode) {
-  const product = await getInformation(productCode)
-  const allergens = document.createElement('p')
-  if (product.product.allergens_from_ingredients === '') {
-    allergens.innerText = "produit ne contient pas d'allergènes"
-  } else {
-    allergens.innerText = ` Les ingredient allergènes du produit : \n ${product.product.allergens_from_ingredients}`
-  }
-  showInformation.appendChild(allergens)
+    const product = await getInformation(productCode)
+    const allergens = document.createElement('p')
+    if(product.product.allergens_from_ingredients === ''){
+        allergens.innerText = "produit ne contient pas d'allergènes"
+    }else{
+        allergens.innerText=` Les ingredient allergènes du produit : \n ${product.product.allergens_from_ingredients}`
+    }
+    showInformation.appendChild(allergens)
 }
 
 // alertAllergens()///////////////////////////////////////
@@ -142,27 +127,8 @@ async function ingredients(productCode) {
     
    console.log("Tableau des ingrédients :", ingredientInfo);
 
-const ctx = document.getElementById('myChart');
     const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
   new Chart(ctx, {
     type: 'doughnut',
     data: {
@@ -218,32 +184,3 @@ async function analysisTags(productCode) {
 
  
 //alertAllergens()
-
-const code = 3270190217190;
-
-async function analysisTags(productCode) {
-  const product = await getInformation(productCode);
-  const tagVeg = document.createElement('p');
-  const produit = product.product;
-  const tags = produit.ingredients_analysis_tags || [];
-  const estVegan = tags.includes("en:vegan");
-  const estVegetarien = tags.includes("en:vegetarian");
-
-  if (estVegan) {
-    tagVeg.innerHTML = "Ce produit est <strong>VEGAN</strong>";
-    console.log(estVegan)
-  } else if (estVegetarien) {
-    tagVeg.innerHTML = "Ce produit est <strong>VÉGÉTARIEN</strong>";
-  } else {
-    tagVeg.innerHTML = "Ce produit n'est ni végétarien ni végan";
-  }
-  showInformation.appendChild(tagVeg);
-}
-
-displayImg(code);
-displayIngredients(code);
-displayGrade(code);
-packaging(code);
-alertAllergens(code);
-analysisTags(code);
-
